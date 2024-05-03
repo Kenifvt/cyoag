fb4a0b341358b2a6a98ba4e359fba95a277567a9
Kenneth Tecson <158852681+Kenifvt@users.noreply.github.com>
2024-05-03 13:51:57 +0100

    Add files via upload

---
+++ audio.js
@@ -0,0 +1,30 @@
+function play_wood_cut() {
+    const audio = document.getElementById("wood_cut_audio");
+    audio.play();
+}
+
+function play_forest() {
+    const audio1 = document.getElementById("forest_audio");
+        audio1.play();
+}
+
+function play_portal_sound() {
+    const audio2 = document.getElementById("portal_audio");
+
+    audio2.play();
+}
+
+function play_whoosh_sound() {
+    const audio3 = document.getElementById("whoosh_audio");
+    audio3.play();
+}
+
+function play_grunt_sound() {
+    const audio4 = document.getElementById("grunt_audio");
+    audio4.play();
+}
+
+function play_fire_sound() {
+    const audio5 = document.getElementById("fire_audio");
+    audio5.play();
+}
